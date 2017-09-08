bfcf5ff8e09f74be60966b03f3d21a5bcc1db000
Ewolly <ewollypop@gmail.com>
2017-09-08 14:12:46 +1200

    removed an =

--- esp/usb-spi/main/main.c
+++ esp/usb-spi/main/main.c
@@ -75,7 +75,7 @@ void audio_record(audio_bus_t speaker, audio_bus_t mic)
 
         words_waiting = audio_recorded_words_waiting(mic);
         //ESP_LOGI(TAG, "time running: %d seconds", audio_get_recording_time(mic));
-        while (words_waiting >= 256) {
+        while (words_waiting > 256) {
             for (int x = 0; x < 256; x++) {
                 word = audio_recorded_read_word(mic);
                 // audio_data[x*2] = word >> 8;
